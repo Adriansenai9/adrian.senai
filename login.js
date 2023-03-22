d832cc968307cfb8c1e199e3afff27a1e57b3049
Adriansenai9 <128429963+Adriansenai9@users.noreply.github.com>
2023-03-22 19:55:33 -0300

    Update login.js

--- login.js
+++ login.js
@@ -109,7 +109,7 @@ function enviarDados() {
         let usuario = dados[index];
         if ((nome === usuario.nome) && (senha === usuario.senha)) {
             localStorage.setItem("nome", nome)
-            window.location.assign("file:///C:/telaDeLogin/home/home.html");
+            window.location.href("./home.html");
             break; // para interromper o loop após encontrar uma correspondência
         } 
     } 
